e5d1bc4687afc4467304ea4e6c37496b7047e8ca
sheppydepp <112518680+sheppydepp@users.noreply.github.com>
2024-02-18 17:08:53 -0600

    Update Server.hpp

--- Server.hpp
+++ Server.hpp
@@ -23,7 +23,17 @@ using namespace std;
 
 class Server {
 public:
-
+  const char* get_file_extension(const char* file_name);
+  const char* get_mime_type(const char* file_ext);
+  bool case_insensitive_compare(const char *s1, const char *s2);
+  char* get_file_case_insensitive(const char *file_name);
+  char* url_decode(const char *src);
+  void build_http_response(const char *file_name, 
+                        const char *file_ext, 
+                        char *response, 
+                        size_t *response_len);
+  void* handle_client(void *arg);
+  void startServer();
 private:
 };
 
