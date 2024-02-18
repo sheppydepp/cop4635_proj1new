9dd91614747e72671049b9f5a9a1aea6c5d42b5a
sheppydepp <112518680+sheppydepp@users.noreply.github.com>
2024-02-18 17:56:33 -0600

    Update Server.hpp

--- Server.hpp
+++ Server.hpp
@@ -34,7 +34,7 @@ public:
   const char* get_mime_type(const char* file_ext);
   bool case_insensitive_compare(const char *s1, const char *s2);
   char* get_file_case_insensitive(const char *file_name);
-  char* url_decode(const char *src);
+  static char* url_decode(const char *src);
   void build_http_response(const char *file_name, 
                         const char *file_ext, 
                         char *response, 
