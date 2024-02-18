bf454706c67b55bde702a56408847af7c197af1c
sheppydepp <112518680+sheppydepp@users.noreply.github.com>
2024-02-18 16:55:16 -0600

    Create Server.hpp

---
+++ Server.hpp
@@ -0,0 +1,23 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <regex.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 104857600
+
+#endif
